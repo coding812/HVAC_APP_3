64b35c2079d11fe448acb069b3949a2443031c45
coding812 <85715642+coding812@users.noreply.github.com>
2022-12-06 07:40:06 -0500

    set up calc page and home pages

--- Views/Cap_CalcPage.xaml.cs
+++ Views/Cap_CalcPage.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace HVAC_APP_3.Views;
+﻿using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace HVAC_APP_3.Views;
 
 public partial class Cap_CalcPage : ContentPage
 {
-	public Cap_CalcPage()
+    double voltage;
+    double startAmps;
+    //double total;
+    public Cap_CalcPage()
 	{
 		InitializeComponent();
 	}
+    private void Calc_Mfd(object sender, EventArgs e)
+    {
+        voltage = Convert.ToDouble(volts.Text);
+        startAmps = Convert.ToDouble(amps.Text);
+
+        double total = Math.Round((startAmps * 2652) / voltage);
+        string printTotal = total.ToString();
+
+        mfd.Text = printTotal;
+    }
 }
